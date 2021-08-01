b3aeb48b8bfa23660f986a85a46d46dc2971d65d
Gaspardian Anthony <88245588+Gaspardian@users.noreply.github.com>
2021-08-01 20:33:38 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
  //insert app version from Grandle into the textView.setText
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);/
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         final TextView textVersion = navigationView.getHeaderView(0).findViewById(R.id.textVersion);
         String versionName = BuildConfig.VERSION_NAME;
         textVersion.setText("Version: " + versionName);
